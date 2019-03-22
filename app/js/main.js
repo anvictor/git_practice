de63ed013147a83ca425366ae92efd0d0ec6f2e8
Viktor <anvictor0@gmail.com>
2019-03-22 14:43:37 +0200

    commit (C1)

--- app/js/main.js
+++ app/js/main.js
@@ -1,3 +1,6 @@
 window.onload = function() {
-    // document.getElementById('main').innerText =  'Javascript works!';
+    let p = document.createElement('p');
+    p.setAttribute('style', "color: white")
+    p.innerText = 'изменения в main.js заметны';
+    document.getElementById('side').appendChild(p);
 };
